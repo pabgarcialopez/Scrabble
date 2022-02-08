4c52c7a26f3a55fddcaf164c00e1b001f7a1d0d2
pablogarcialopez <pablogarcialopez@10.211.55.4>
2022-02-08 21:38:39 +0100

    Probando subiendo cosas a una rama distinta de master.
    Signed-off-by: pablogarcialopez <pablogarcialopez@10.211.55.4>

--- Scrabble/src/Main.java
+++ Scrabble/src/Main.java
@@ -4,6 +4,7 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hola Mundo!");
 		System.out.println("Segundo");
+		System.out.println("Cambios en el Main.");
 		
 	}
 
