8a34dce08bc069fc22d518b2ffdd434f3b1f729b
pablna03 <pablna03@ucm.es>
2022-02-11 13:18:12 +0100

    Creado EnumSpecialEffects para las carcateristicas especiales de las casillas. Creada clase GameBox para representar las casillas del juego.
    Signed-off-by: pablna03 <pablna03@ucm.es>

---
+++ Scrabble/src/GameObjects/GameBox.java
@@ -0,0 +1,13 @@
+package GameObjects;
+
+import SpecialEffects.SpecialEffects;
+
+public class GameBox {
+
+	private Tile tile;
+	private int x;
+	private int y;
+	private SpecialEffects specialEffect;
+	
+
+}
---
+++ Scrabble/src/SpecialEffects/SpecialEffects.java
@@ -0,0 +1,6 @@
+package SpecialEffects;
+
+public enum SpecialEffects {
+
+	DOUBLE_LETTER, TRIPLE_LETTER, DOUBLE_WORD, TRIPLE_WORD;
+}
