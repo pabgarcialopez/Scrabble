ad0db12f334c42baea54a141e9bcb72db3e4471c
pablna03 <pablna03@ucm.es>
2022-02-11 12:59:19 +0100

    He creado dos metodos de añadir una ficha y eliminarla.
    Signed-off-by: pablna03 <pablna03@ucm.es>

--- Scrabble/src/GameTiles.java
+++ Scrabble/src/GameTiles.java
@@ -8,4 +8,14 @@ public class GameTiles {
 	GameTiles() {
 		this.tiles = new ArrayList<Tile>();
 	}
+	
+	private void add(Tile tile) {
+		this.tiles.add(tile);
+	}
+	
+	private void remove(Tile tile) {
+		this.tiles.remove(tile);
+	}
+	
+	
 }
