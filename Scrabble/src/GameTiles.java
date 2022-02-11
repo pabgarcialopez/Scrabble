469cb36465ac9d8706883486eadf95a5c674a3c8
pablna03 <pablna03@ucm.es>
2022-02-11 12:52:27 +0100

    Creada clase Tile y GameTiles.
    Signed-off-by: pablna03 <pablna03@ucm.es>

---
+++ Scrabble/src/GameTiles.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class GameTiles {
+
+	private List<Tile> tiles;
+	
+	GameTiles() {
+		this.tiles = new ArrayList<Tile>();
+	}
+}
---
+++ Scrabble/src/Tile.java
@@ -0,0 +1,11 @@
+
+public class Tile {
+
+	private String letter;
+	private int points;
+	
+	Tile(String letter, int points) {
+		this.letter = letter;
+		this.points = points;
+	}
+}
